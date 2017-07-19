c187aaec0b1bb6a100d091679b94c36ed27ddcec
Grigory Tantsevov <tantsevov@gmail.com>
2017-07-20 01:17:59 +0300

    Now server return file or error

--- boost_as/boost_as/tcp_connection.cpp
+++ boost_as/boost_as/tcp_connection.cpp
@@ -3,6 +3,9 @@
 #include <boost/bind.hpp>
 #include <iostream>
 #include <boost/filesystem.hpp>
+#include <sstream>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <sys/sendfile.h>
 
 using namespace boost::filesystem;
 
@@ -22,34 +25,84 @@ void TcpConnection::doABarrelRoll()
 void TcpConnection::handleConnect(boost::asio::yield_context yield)
 {
     try {
+        int file;
         boost::asio::streambuf buffer(1024);
         boost::asio::async_read_until(*m_sock, buffer, "\n", yield);
         std::istream is(&buffer);
         std::string line;
         std::getline(is, line);
 
-        std::string filename = getFileName(line);
+        std::string protocol, trash;
+        std::istringstream ist(line);
+        ist >> trash >> m_filename >> protocol;
+
+        m_filename = getFileName(m_filename);
+
+        int requestCode = 200;
 
         /// Whell, whell, whell. What do we have there
-        if (filename.empty()) {
+        if (m_filename.empty()) {
             std::cerr << "It's not get request\n";
-            return;
-        }
-        if (!youWantFileOrWhat(filename)) {
+            requestCode = 400;
+        } else if (!youWantFileOrWhat()) {
             std::cerr << "There is no such file\n";
-            return;
+            requestCode = 404;
+        } else if ((file = open(m_filename.c_str(), O_RDONLY)) == -1) {
+            std::cerr << "Can't  open file\n";
+            requestCode = 500;
         }
 
-        int file;
-        if ((file = open(filename.c_str(), O_RDONLY)) == -1) {
-            std::cerr << "Can't open this file\n";
-            return;
+        std::cout << "File request: " << m_filename << std::endl;
+
+        std::string headers(answerHeaders(protocol, requestCode));
+        std::vector<char> headersVector(headers.c_str(), headers.c_str() + headers.length() + 1);
+        boost::asio::async_write(*m_sock, boost::asio::buffer(&headersVector[0], headersVector.size()), yield);
+
+        switch (requestCode) {
+        case 200:
+        {
+            off_t offset = 0;
+            int send = 0;
+            path pathToFile(m_filename);
+            int length = file_size(pathToFile);
+            m_sock->async_write_some(boost::asio::null_buffers(), yield);
+            send = sendfile(m_sock->native(), file, &offset, length);
+            if (send < 0)
+                std::cerr << "Fail in sending file\n";
+            break;
         }
+        case 400:
+        {
+            const char* error400 = "<html> 400 Unable to process request  </html>";
+            std::cerr << error400 << std::endl;
+            boost::asio::async_write(*m_sock, boost::asio::buffer(error400, strlen(error400)), yield);
+            break;
+        }
+        case 404:
+        {
+            const char* error404 = "<html> 404 File not found </html>";
+            std::cerr << error404 << std::endl;
+            boost::asio::async_write(*m_sock, boost::asio::buffer(error404, strlen(error404)), yield);
+            break;
+        }
+        case 500:
+        {
+            const char* error500 = "<html> 500 Can't open file </html>";
+            std::cerr << error500 << std::endl;
+            boost::asio::async_write(*m_sock, boost::asio::buffer(error500, strlen(error500)), yield);
+            break;
+        }
+        default:
+            break;
+        }
+
         close(file);
 
-        std::cout << filename << std::endl;
+        closeConnect();
+
+        std::cout << headers << std::endl;
     } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "Connect exception: " << e.what() << std::endl;
     }
 }
 
@@ -59,31 +112,83 @@ std::__cxx11::string TcpConnection::getFileName(std::__cxx11::string req)
     if (pos == std::string::npos)
         return std::string();
     std::string withoutGet = req.substr(pos + 4);     /// length("GET ") is 4
-    std::string filenameInUrl;
-    std::istringstream ist(withoutGet);
-    ist >> filenameInUrl;
 
     std::string ret;
 
-    for (size_t i = 0; i < filenameInUrl.length(); ++i) {
-        if (filenameInUrl[i] == '%') {
+    for (size_t i = 0; i < withoutGet.length(); ++i) {
+        if (withoutGet[i] == '%') {
             int val;
-            sscanf(filenameInUrl.substr(i + 1, 2).c_str(), "%x", &val);
+            sscanf(withoutGet.substr(i + 1, 2).c_str(), "%x", &val);
             ret += static_cast<char>(val);
             i += 2;
-        } else if (filenameInUrl[i] == '+') {
+        } else if (withoutGet[i] == '+') {
             ret += ' ';
         } else {
-            ret += filenameInUrl[i];
+            ret += withoutGet[i];
         }
     }
 
     return ret;
 }
 
-bool TcpConnection::youWantFileOrWhat(std::__cxx11::string filename)
+bool TcpConnection::youWantFileOrWhat()
 {
 
-    path pathToFile(filename);
+    path pathToFile(m_filename);
     return (is_regular_file(pathToFile) and exists(pathToFile));
+}
+
+std::__cxx11::string TcpConnection::answerHeaders(std::__cxx11::string protocol, int code)
+{
+    std::ostringstream ret;
+
+    ret << protocol;
+    switch (code) {
+    case 200:
+        ret << " 200 OK\r\n";
+        break;
+    case 400:
+        ret << " 400 Bad Request\r\n";
+        break;
+    case 404:
+        ret << " 404 Not Found\r\n";
+        break;
+    case 500:
+        ret << " 500 Internal Server Error\r\n";
+        break;
+    default:
+        break;
+    }
+
+    ret.imbue(std::locale(ret.getloc(), new boost::posix_time::time_facet("%a, %d %b %Y %H:%M:%S GMT")));
+    ret << "Server: FileServer/1.0 (Linux) \r\n"
+            << "Date: " << boost::posix_time::second_clock::universal_time() << "\r\n"
+            << "Connection: close\r\n";
+
+    if (code == 200) {
+        ret << fileHeaders();
+    }
+
+    return ret.str();
+}
+
+std::__cxx11::string TcpConnection::fileHeaders()
+{
+    std::ostringstream ret;
+
+    size_t lastDot = m_filename.find_last_of(".");
+    std::string ex = m_filename.substr(lastDot + 1);
+
+    ret << "Content-Type: " << m_mimeTypes[ex] << "\r\n";
+    ret << "Content-Disposition: attachment; filename=\"" << m_filename << "\"\r\n";
+    ret << "Content-Length: " << file_size(m_filename) << "\r\n";
+
+    return ret.str();
+}
+
+void TcpConnection::closeConnect()
+{
+    boost::system::error_code error = boost::asio::error::interrupted;
+    for (; m_sock->is_open() && error == boost::asio::error::interrupted;)
+        m_sock->close(error);
 }
--- boost_as/boost_as/tcp_connection.h
+++ boost_as/boost_as/tcp_connection.h
@@ -19,9 +19,30 @@ private:
     void handleConnect(boost::asio::yield_context yield);
     std::string getFileName(std::string req);
 
-    bool youWantFileOrWhat(std::string filename);
+    bool youWantFileOrWhat();
+
+    std::string answerHeaders(std::string protocol, int code);
+    std::string fileHeaders();
+
+    void closeConnect();
 
     std::shared_ptr<tcp::socket> m_sock;
+    std::map<std::string, std::string> m_mimeTypes = {
+        {"zip", "application/zip"},
+        {"txt", "text/plain"},
+        {"html", "text/html"},
+        {"css", "text/css"},
+        {"csv", "text/csv"},
+        {"js", "application/javascript"},
+        {"jpg", "image/jpeg"},
+        {"jpeg", "image/jpeg"},
+        {"gif", "image/gif"},
+        {"png", "image/png"},
+        {"gif", "image/gif"},
+        {"swf", "application/x-shockwave-flash"},
+        {"iso", "application/octet-stream"}
+    };
+    std::string m_filename;
 };
 
 #endif // TCP_CONNECTION_H
